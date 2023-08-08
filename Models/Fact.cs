5b1af73a635055e18de0031ebbdae4d8f7b38426
Kostas <64689371+bebeto1914@users.noreply.github.com>
2023-08-08 16:37:20 +0100

    Add files via upload

---
+++ Models/Fact.cs
@@ -0,0 +1,16 @@
+﻿namespace ScientificQuestionsApp.Models
+{
+    public class Fact
+    {
+        public int Id { get; set; }
+
+        public string ScientificQuestion { get; set; }
+
+        public string ScientificAnswer { get; set; }
+
+        public Fact()
+        {
+
+        }
+    }
+}
